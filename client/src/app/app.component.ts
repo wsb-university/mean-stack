11a5e62decf1d616c2fa1b4ec60d0ba6f66fe994
medynski <wojciech@medynski.io>
2020-06-13 23:52:29 +0200

    Content and styles adjustment

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { flipInX } from 'ng-animate';
+import { TextAnimation } from 'ngx-teximate';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'client';
+
+  enterAnimation: TextAnimation = {
+    animation: flipInX,
+    delay: 50,
+    type: 'letter',
+  };
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import { TeximateModule } from 'ngx-teximate';
 import { AdminComponent } from './admin/admin.component';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { PostComponent } from './post/post.component';
 import { PostsComponent } from './posts/posts.component';
-import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -24,8 +26,20 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [AppComponent, PostsComponent, PostComponent, AdminComponent, LoginComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes), FormsModule],
+  declarations: [
+    AppComponent,
+    PostsComponent,
+    PostComponent,
+    AdminComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    BrowserAnimationsModule,
+    TeximateModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
